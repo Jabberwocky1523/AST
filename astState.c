dddbcad4caa2946bf82c1e0ad875442f70550a29
jabberwocky <2004040505ab>
2024-12-18 21:38:28 +0800

    add ast_String

--- astState.c
+++ astState.c
@@ -5,10 +5,4 @@ ast_State *ast_NewState(ast_Alloc func, void *ud)
 {
     ast_State *L;
     global_State *g;
-    void *l = (*func)(ud, NULL, 0, sizeof(global_State));
-    if (l == NULL)
-    {
-        return NULL;
-    }
-    L = (ast_State *)l;
 }
